98d05ccb31615011660bafdc576564a863162ff1
Umberto Sonnino <rimher@gmail.com>
2019-11-15 13:26:35 +0000

    Add tests

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Umberto Sonnino on 11/15/19.
+//  Copyright © 2019 Umberto Sonnino. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- SwiftyLibTests/SwiftyLibTests.swift
+++ SwiftyLibTests/SwiftyLibTests.swift
@@ -11,24 +11,18 @@ import XCTest
 
 class SwiftyLibTests: XCTestCase {
 
+    var swiftyLib: SwiftyLib!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLib = SwiftyLib()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 2), 3)
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 1, b: 2), -1)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
